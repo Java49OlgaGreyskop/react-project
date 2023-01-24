e16646a4e7ee5531cbe505b8eb5fdbd16ffa879f
Olga greyskop <o.greyskop@gmail.com>
2023-01-24 13:08:09 +0200

    error divs

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
+import { Input } from './components/Input';
 import { Timer } from './components/Timer';
 
 
+
+
+
 function App() {
-  const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column" }
-  const flexRow: React.CSSProperties = { display: "flex", flexDirection: "row" , 
-  justifyContent: "space-around", width: "50vw", marginTop: "4vh"}
-  return <div style={flexColumn}>
-    <div style={flexRow}>
+  const properties: React.CSSProperties = {
+    display: "flex", flexWrap: "wrap"
+  }
+  const [cities, SetCities] = React.useState<string[]>([])
+  function creatingDivs(value: string): string {
+    const cities: string[] = value.split("#");
+    let res = '';
+    SetCities(cities.slice());
+    if (cities.length % 2 !== 0) {
+      res = "error with a proper message";
+    }
+    else {
+
+      getDivs;
+
+    }
+    return res;
+  }
+  function getDivs(cities: string[]): JSX.Element[] {
+    return cities.map(cities => <div style={{
+      display: "flex", flexDirection: "column",
+      justifyContent: "space-around", width: "50vw", marginTop: "4vh"
+    }}>
       <Timer cityOrCountry="London"></Timer>
       <Timer cityOrCountry="Paris"></Timer>
-    </div>
-    <div style={flexRow}>
-      <Timer cityOrCountry="India "></Timer>
-      <Timer cityOrCountry={"Cuba"}></Timer>
-    </div>
-
-  </div>
-
+    </div>)
+  }
+   return <section>
+   <Input placeHolder={`“enter cities/countries separated by #`}inputProcess={creatingDivs}></Input>
+   </section>
 }
+
 
 export default App;
