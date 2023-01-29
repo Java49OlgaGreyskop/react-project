cb9e1bcd040ebe9df79a17036beea531a0295670
Olga greyskop <o.greyskop@gmail.com>
2023-01-29 16:41:12 +0200

    HW#5 start game

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
 import React from 'react';
-import { Input } from './components/Input';
-import { Timer } from './components/Timer';
+import {  getRandomMatrix, getRandomNumber, getRandomArrayElement, getRandomDate,  } from './components/utils/random';
 
 
 function App() {
-  const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column" }
-  const flexRow: React.CSSProperties = { display: "flex", flexDirection: "row" , 
-  justifyContent: "space-around", width: "50vw", marginTop: "4vh"}
-  const [cityCountries, setCityCountries] = React.useState<string[]>([]);
-  function inputProcessFun(value: string): string {
-    const cityCountriesFromInput: string[] = value.split("#");
-    let res:string = '';
-    if(cityCountriesFromInput.length % 2 != 0) {
-      res = "There should be even number of the country/cities"
-    } else {
-        setCityCountries(cityCountriesFromInput.slice());
-    }
-    return res;
-  }
-  function getTimers(): JSX.Element[] {
-    const res: JSX.Element[] = [];
-    for(let i = 0; i < cityCountries.length; i+=2) {
-      res.push(<div style={flexRow}>
-        <Timer cityOrCountry={cityCountries[i]} />
-        <Timer cityOrCountry={cityCountries[i + 1]} />
-      </div>)
-    }
-
-    return res;
-  }
-
-  return <div style={flexColumn}>
-    <Input placeHolder={'enter city/countries separated by #'}
-     inputProcess={inputProcessFun}/>
-     {getTimers()}
+  
+console.log (getRandomNumber(1,6,false, true))
+console.log (getRandomMatrix(10,5,1,10))
+console.log(getRandomArrayElement([1,2,3,5,1000, "abc","cccCAGe"]))
+console.log(getRandomDate(1990,2005))
+ 
+ return <div >
     
-
   </div>
 
 }
