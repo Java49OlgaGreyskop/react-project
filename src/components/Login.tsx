a872507a65731e546af31634c03b94da237cbaa1
Olga greyskop <o.greyskop@gmail.com>
2023-02-08 01:02:19 +0200

    HW#39 UserName

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,29 @@
 import { authActions } from "../redux/authSlice";
-import {useDispatch} from 'react-redux'
-export const Login: React.FC = ()=>{
+import React from "react";
+import {useDispatch} from 'react-redux';
+import { Input } from "./Input";
+ type Props={
+    loginVal:(userName: string)=> boolean
+ }
+export const Login: React.FC<Props> =(props)=>{
+    
+    const [userName, setUsername]= React.useState('');
     const dispatch = useDispatch();
-    return <button onClick={() => dispatch(authActions.login())}>Login</button>
+
+    function getName (value:string): string {
+if (props.loginVal(value)){
+    setUsername(value);
 }
+return '';
+}
+return <div>
+     <Input placeHolder={'Enter user name'} inputProcess={getName}></Input>
+     <p> user name: {userName}</p>
+        
+        <button onClick={() => dispatch(authActions.login(userName))}>Login</button>
+</div>
+    }
+    
+
+
+    
